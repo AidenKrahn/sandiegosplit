8687f4e4ed569ece2d7e177f63cac012163c5c5f
AidenKrahn <125599022+AidenKrahn@users.noreply.github.com>
2023-04-20 14:06:42 -0600

    Add files via upload

---
+++ sandiego/blsp.py
@@ -0,0 +1,20 @@
+
+
+bolivia = [1, 1, "stripes, a crest", "paraguay", "natural gas", "12 million", "Valle de La Luna"]
+chile = [3, 2 ,"star(s), French colours", 'argentina', "copper and fish", "19.5 million", "Easter Island"]
+peru = [3, 3, "stripes, a crest", 'ecuador', 'coffee', "33.7 million", "Machu Picchu"]
+brazil = [0, 4, "star(s), blue", 'venezuela', 'soy beans and sugar', "214.3 million", "Christ the Redeemer"]
+argentina = [2, 5, "stripes, blue", 'chile', 'soy beans and corn', "45.8 million", "Bariloche"]
+uruguay = [2, 6, "stripes, blue", 'paraguay', 'meat and pharmaceuticals', "3.4 million", "Fingers of Punta del Este"]
+paraguay = [1, 7, "stripes, a crest", 'uruguay', 'soy beans, soy oil', "6.7 million", "Monday Falls"]
+venezuela = [1, 8, "stars, a crest", 'bolivia', 'steel, aluminum', "28.2 million", "Angel Falls"]
+colombia = [1, 9, "stripes, blue", 'venezuela', 'natural gas, coffee', "51.5 million", "Salt Cathedral"]
+ecuador = [2, 10, "stripes, a crest", 'peru', 'shrimp, bananas', "17.8 million", "Middle of the World City"]
+guyana = [1, 11, "triangles, green", 'colombia', 'oil, gold', ".8 million", "Kaieteur Falls"]
+frenchguiana = [1, 12, "star(s), triangles", 'guyana', 'shrimp, gold', ".3 million", "Devil's Island"]
+suriname = [1, 13, "star(s), stripes", 'guyana', 'rice, bananas', ".6 million", "Jodensavanne"]
+listofcountries = ["bolivia", "chile", "peru", "brazil", "argentina", "uruguay",
+                   "paraguay", "venezuela", "colombia", "ecuador", "guyana", "frenchguiana", "suriname"]
+fakecountry = [17]
+listofcountriespart2 = [bolivia, chile, peru, brazil, argentina, uruguay,
+                   paraguay, venezuela, colombia, ecuador, guyana, frenchguiana, suriname]
